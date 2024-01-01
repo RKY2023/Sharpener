5e43ff2490bafb064dfffaef24c09e5c928250de
Raj kumar <xraj2023@gmail.com>
2024-01-01 14:26:43 +0530

    React expense location

--- reactJS/myapp/src/App.js
+++ reactJS/myapp/src/App.js
@@ -7,12 +7,14 @@ function App() {
       title: "Toilet ",
       amount: 94.12,
       date: new Date(2024, 0, 1),
+      location : 'New York',
     },
     {
       id: "e2",
       title: "car Sur",
       amount: 124.12,
       date: new Date(2024, 0, 2),
+      location : 'Manhatttan',
     },
   ];
   return (
@@ -26,11 +28,13 @@ function App() {
             title={expenses[0].title}
             amount={expenses[0].amount}
             date={expenses[0].date}
+            LocationOfExpenditure={expenses[0].location}
           />
           <ExpenseItem
             title={expenses[1].title}
             amount={expenses[1].amount}
             date={expenses[1].date}
+            LocationOfExpenditure={expenses[1].location}
           />
         
         {/* <a
--- reactJS/myapp/src/components/ExpenseItem.js
+++ reactJS/myapp/src/components/ExpenseItem.js
@@ -4,14 +4,17 @@ import './ExpenseItem.css';
 
 
 function ExpenseItem(props){
-    
+    const LocationOfExpenditure = 'manhattan';
 
     // return <h2>Expense item 1</h2>
     return (
         <div className='expense-item'>
             <ExpenseDate date={props.date} />
             <div className='expense-item__description'>
-                <h2>{props.title}</h2>
+                <h2>{props.title}
+                <div className='expense-item__description'>{LocationOfExpenditure}</div>
+                </h2>
+                
                 <div className='expense-item__price'>${props.amount}</div>    
             </div>
         </div>
