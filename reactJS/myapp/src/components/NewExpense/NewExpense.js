5e484d50e5e34e531ec3c2fa205b7f37c1134962
Raj kumar <xraj2023@gmail.com>
2024-01-07 17:28:43 +0530

    DIY Conditional Rendering

--- reactJS/myapp/src/components/NewExpense/NewExpense.js
+++ reactJS/myapp/src/components/NewExpense/NewExpense.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 
 import ExpenseForm from "./ExpenseForm";
 import './NewExpense.css';
 
 const NewExpense = (props) => {
+    const [showForm, setShowForm] = useState(0);
+
     const saveExpenseDataHandler = (enteredExpenseData) =>{
         const expenseData = {
             ...enteredExpenseData,
@@ -11,12 +13,27 @@ const NewExpense = (props) => {
         };
         console.log(expenseData);
         props.onAddExpense(expenseData);
+        setShowForm(0);
     };
+    
+    const openAddExpense = (event) => {
+        event.preventDefault();
+        setShowForm(1);
+    };
+
+    if(showForm == 1)
+        return (
+            <div className="new-expense">
+                <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}/>
+            </div>
+    );
+
     return (
         <div className="new-expense">
-            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}/>
+            <button type="submit" onClick={openAddExpense}>Add New Expense</button>
         </div>
-    );
+    )
+    
 };
 
 export default NewExpense;
