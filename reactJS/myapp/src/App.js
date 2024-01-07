f383b503e0cf7643a0dbfb80641c5ff70ec83875
Raj kumar <xraj2023@gmail.com>
2024-01-07 15:00:24 +0530

    dynamic ELement

--- reactJS/myapp/src/App.js
+++ reactJS/myapp/src/App.js
@@ -1,32 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
+const INITIAL_EXPENSES = [
+  {
+    id: "e1",
+    title: "Toilet ",
+    amount: 94.12,
+    date: new Date(2024, 0, 1),
+    location : 'New York',
+  },
+  {
+    id: "e2",
+    title: "car Sur",
+    amount: 124.12,
+    date: new Date(2024, 0, 2),
+    location : 'Manhatttan',
+  },
+];
+
 const App = () => {
-  let expenses = [
-    {
-      id: "e1",
-      title: "Toilet ",
-      amount: 94.12,
-      date: new Date(2024, 0, 1),
-      location : 'New York',
-    },
-    {
-      id: "e2",
-      title: "car Sur",
-      amount: 124.12,
-      date: new Date(2024, 0, 2),
-      location : 'Manhatttan',
-    },
-  ];
+  const [expenses, setExpenses] = useState(INITIAL_EXPENSES);
 
   const addExpenseHandler = expense => {
-    console.log('in app.js');
+    // console.log('in app.js');
     
-    expenses = [...expenses, expense];
-    console.log(expenses);
-  }
+    // expenses = [...expenses, expense];
+    // console.log(expenses);
+    setExpenses(prevExpenses => {
+      return [expense, ...prevExpenses]
+    });
+  };
 
   return (
     <div>
