40f6bf41fe958051ba367870f3fd70a4468d995a
Raj kumar <xraj2023@gmail.com>
2024-01-26 21:52:43 +0530

    ta

--- reactJS/meals/src/components/Cart/Cart.js
+++ reactJS/meals/src/components/Cart/Cart.js
@@ -11,7 +11,7 @@ const Cart = props => {
                 price: 12.99,
             }].map((item) => (
                 <li>{item.name}</li>
-            ))}
+            ))}ss
         </ul>
     );
 
