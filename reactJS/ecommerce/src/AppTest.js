3bbb47d25d6938c81fea191516e6699d9e42157e
Raj kumar <xraj2023@gmail.com>
2024-02-02 12:47:55 +0530

    movies

--- reactJS/ecommerce/src/AppTest.js
+++ reactJS/ecommerce/src/AppTest.js
@@ -17,6 +17,8 @@ const AppTest = (props) => {
     4. Implement the About page in the most optimised way 2 hr
     5. Implement the Home page 2 hr
     6. Making Api calls in React 90 mins
+    7. Loading the application 1hr
+    8. 
 dsd
 
         {/* </Stack>; */}
