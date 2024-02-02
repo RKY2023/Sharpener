2d197a63a59715bcdef66705bb85ed9151ab54e3
Raj kumar <xraj2023@gmail.com>
2024-02-02 10:57:58 +0530

    6

--- reactJS/ecommerce/src/AppTest.js
+++ reactJS/ecommerce/src/AppTest.js
@@ -11,6 +11,12 @@ const AppTest = (props) => {
             <Button as="a" variant="success">
                 Button as link
             </Button>
+            1. Building Block of Ecommmerce 90 mins
+    2. Cart Component 2 hr 
+    3. Making Add to Cart Work 90 mins
+    4. Implement the About page in the most optimised way 2 hr
+    5. Implement the Home page 2 hr
+    6. 
         {/* </Stack>; */}
         </>
     )
