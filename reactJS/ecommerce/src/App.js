f70730eb5e4386dbf8160b7a9d3f5a632e0eabf1
Raj kumar <xraj2023@gmail.com>
2024-02-02 18:37:23 +0530

    router

--- reactJS/ecommerce/src/App.js
+++ reactJS/ecommerce/src/App.js
@@ -1,13 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './components/Layout/Header';
 import AppTest from './AppTest';
+
+import { Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <AppTest />
+      <Route />
     </div>
   );
 }
