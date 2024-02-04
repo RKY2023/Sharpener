f1e34f6a5ed44e7e3d97d7b400a9b5efef9c6745
Raj kumar <xraj2023@gmail.com>
2024-02-05 00:11:47 +0530

    login done

--- reactJS/authentication-main/src/components/Auth/AuthForm.js
+++ reactJS/authentication-main/src/components/Auth/AuthForm.js
@@ -1,11 +1,15 @@
 import { useState, useRef } from 'react';
 
 import classes from './AuthForm.module.css';
+import { Prompt } from 'react-router-dom';
 
 const AuthForm = () => {
   const [isLogin, setIsLogin] = useState(true);
   const emailInputRef = useRef();
   const passwordInputRef = useRef();
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [isEmailExist, setIsEmailExist] = useState(false);
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
@@ -17,6 +21,32 @@ const AuthForm = () => {
     const enteredPassword = passwordInputRef.current.value;
 
     if(isLogin){
+      fetch('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyB8_J6A_7bsjzl4Zy3OkODi-GMz9MftKyY',
+        {
+          method: 'POST',
+          body: JSON.stringify({
+            email: enteredEmail,
+            password: enteredPassword,
+            returnSecureToken: true,
+          }),
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        }
+      ).then((res) => {
+        if(res.ok) {
+          return res.json().then( (data) => {
+            console.log(data);
+          });
+        } else {
+          return res.json().then( (data) => {
+            console.log(data.error);
+            if(data.error.message=='INVALID_LOGIN_CREDENTIALS'){
+
+            }
+          });
+        }
+      });
 
     }else{
       fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyB8_J6A_7bsjzl4Zy3OkODi-GMz9MftKyY',
@@ -33,18 +63,26 @@ const AuthForm = () => {
         }
       ).then( (res) => {
         if(res.ok) {
-          //
-        } else {
           return res.json().then( (data) => {
             console.log(data);
+          });
+        } else {
+          return res.json().then( (data) => {
+            console.log(data.error.code, data.error.message);
+            if(data.error.code == 400 && data.error.message=='EMAIL_EXISTS'){   
+              setIsEmailExist(true);
+              switchAuthModeHandler();
+            }
           });
         }
       });
     }
   };
+  
 
   return (
     <section className={classes.auth}>
+      <Prompt when={!isEmailExist} message='Email Already Exists' />
       <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
       <form onSubmit={submitHandler}>
         <div className={classes.control}>
@@ -60,14 +98,10 @@ const AuthForm = () => {
             required
           />
         </div>
-          <button
-            type='button'
-            className={classes.toggle}
-            onClick={switchAuthModeHandler}
-          >
-            {isLogin ? 'Create new account' : 'Login with existing account'}
-          </button>
         <div className={classes.actions}>
+          <button>
+            {isLogin ? 'Login':'Create Account'}
+          </button>
           <button
             type='button'
             className={classes.toggle}
