892e72665b04906b917e414d2e40884eb21c5a1a
Raj kumar <xraj2023@gmail.com>
2024-02-11 19:27:22 +0530

    expense

--- reactJS/expensetracker/src/App.js
+++ reactJS/expensetracker/src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import { ReactDOM } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePg from './pages/HomePg';
+import AuthPg from './pages/AuthPg';
 
-function App() {
+const App = (props) => {
   return (
-    <div className="App">
-
-    </div>
+    <>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<AuthPg />} />
+        <Route path='/' element={HomePg} exact />
+        <Route path='/auth' element={AuthPg} />
+        <Route path='*' element={HomePg} />
+      </Routes>
+    </BrowserRouter>
+    </>
   );
 }
 
