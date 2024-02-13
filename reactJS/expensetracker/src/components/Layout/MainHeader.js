936bddb1e419014d937ef8be80c8250678d63c91
Raj kumar <xraj2023@gmail.com>
2024-02-13 14:15:37 +0530

    7; Logout user

--- reactJS/expensetracker/src/components/Layout/MainHeader.js
+++ reactJS/expensetracker/src/components/Layout/MainHeader.js
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Button, Navbar, Nav, Container } from 'react-bootstrap';
 import ProfileStatus from '../Profile/ProfileStatus';
-import { Switch } from 'react-router-dom/cjs/react-router-dom';
+import { Switch, useHistory } from 'react-router-dom/cjs/react-router-dom';
 import ProfileUpdate from '../Profile/ProfileUpdate';
 import ProfileData from '../Profile/ProfileData';
+import ExpenseContext from '../../store/ExpenseContext';
 
 const MainHeader = (props) => {
+    const history = useHistory();
+    const authCtx = useContext(ExpenseContext);
+
+    const logoutHandler = (e) => {
+        e.preventDefault();
+        authCtx.logout();
+        history.replace('/auth');
+    }
+
     return (
     <>
     <div className='p-5'>
         <Navbar>
             <Navbar.Brand>Welcome to Expense Tracker</Navbar.Brand>
             <ProfileStatus />
+            <Button className='m-3 btn-danger' onClick={logoutHandler}>Logout</Button>
         </Navbar>
         <hr></hr>
     </div>
