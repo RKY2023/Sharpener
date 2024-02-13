7ad1e5f50e248d4305e2feb990a4a4e67b2a8ad1
Raj kumar <xraj2023@gmail.com>
2024-02-13 18:51:15 +0530

    11; Introduction to Redux

--- reactJS/tryRedux/redux-demo.js
+++ reactJS/tryRedux/redux-demo.js
@@ -1,16 +1,16 @@
 const redux = require('redux');
 
 const counterReducer = (state = { counter: 0 }, action) => {
-    if(action.type == 'increment'){
+    // if(action.type == 'increment'){
         return {
             counter: state.counter +1
         };
-    }else if(action.type == 'decrement'){
-        return {
-            counter: state.counter -1
-        };
-    }
-    
+    // }else if(action.type == 'decrement'){
+    //     return {
+    //         counter: state.counter -1
+    //     };
+    // }
+    // return state;
 };
 
 const store = redux.createStore(counterReducer);
@@ -25,4 +25,4 @@ const counterSubscriber = () => {
 store.subscribe(counterSubscriber);
 
 store.dispatch({type : 'increment' });
-store.dispatch({type : 'increment' });store.dispatch({type : 'increment' });store.dispatch({type : 'increment' });store.dispatch({type : 'increment' });
+// store.dispatch({type : 'increment' });
