c16e48eda06cbb0ee4b735b8bf167f68794e1a8d
Raj kumar <xraj2023@gmail.com>
2024-02-14 16:01:10 +0530

    tt

--- reactJS/expensetracker/src/App.test.js
+++ reactJS/expensetracker/src/App.test.js
@@ -8,3 +8,12 @@ test('renders learn react link', () => {
   expect(linkElement).toBeInTheDocument();
   expect(expensecheck).toBeInTheDocument();
 });
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Welcome/i);
+  const expensecheck = screen.getByText(/expense/i, { exact: true});
+  expect(linkElement).toBeInTheDocument();
+  expect(expensecheck).toBeInTheDocument();
+});
+
