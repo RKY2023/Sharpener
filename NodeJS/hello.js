9258081457ad2425523edc7168a6e390c297bdb8
RKY <xraj2023@gmail.com>
2024-03-10 22:49:18 +0530

    server

--- NodeJS/hello.js
+++ NodeJS/hello.js
@@ -1,1 +1,25 @@
-console.log('Hello World');
+// console.log('Hello World');
+
+// const fs = require('fs');
+// const { REPLServer } = require('repl');
+// fs.writeFileSync('hello.txt','Hello are you there?');
+
+// REPLServer Read  eval Print Loop
+// const product = (x,y) => {
+//     return x*y;
+// }
+// console.log(product(2,3));
+// const obj = {
+//     name: 'Raj',
+//     age: 27,
+// }
+
+// async function fun() {
+//     console.log('a');
+//     console.log('b');
+//     const t= await new Promise((res,rej) => { setTimeout(() => {console.log('c'); res();}, 3000);} );
+//     const ta= await new Promise((res,rej) => { setTimeout(() => {console.log('d'); res();}, 0);} );
+//     console.log('e')
+// }
+// fun();
+
