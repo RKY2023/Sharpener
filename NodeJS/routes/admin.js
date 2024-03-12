eb5af3532b4e7a53c55ec3d2e7f088bf525660fa
RKY <xraj2023@gmail.com>
2024-03-12 16:03:02 +0530

    update files

--- NodeJS/routes/admin.js
+++ NodeJS/routes/admin.js
@@ -1,19 +1,29 @@
 const path = require('path');
-const express = require("express");
+
+const express = require('express');
 
 const rootDir = require('../util/path');
+
 const router = express.Router();
-// routes /admin/add-product => GET
-router.get("/add-product", (req, res, next) => {
-//   res.send(
-//     '<form action="/admin/add-product" method="POST"> <input type="text" name="title" /><input type="text" name="title2" /><button type="submit">Submit</button> </form>'
-//   );
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
-});
-// routes /admin/add-product => POST
-router.post("/add-product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
+
+const products = [];
+
+// /admin/add-product => GET
+router.get('/add-product', (req, res, next) => {
+  res.render('add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true
+  });
 });
 
-module.exports = router;
+// /admin/add-product => POST
+router.post('/add-product', (req, res, next) => {
+  products.push({ title: req.body.title });
+  res.redirect('/');
+});
+
+exports.routes = router;
+exports.products = products;
