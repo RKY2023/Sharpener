0729c63f7f6522c84b04d674e3cc98c63ff0e1b9
RKY <xraj2023@gmail.com>
2024-03-12 10:39:40 +0530

    body-parser;

--- NodeJS/app.js
+++ NodeJS/app.js
@@ -10,27 +10,29 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false}));
 
-app.use('/', (req, res, next) => {
-    res.send('<form action="/product" method="POST"> <input type="submit" name="title"/></form>')
+app.use('/add-product', (req, res, next) => {
+    res.send('<form action="/product" method="POST"> <input type="text" name="title" /><input type="text" name="title2" /><button type="submit">Submit</button> </form>')
+})
+
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
 })
 
 app.use('/', (req, res, next) => {
-    res.send('<form action="/product" method="POST"> <input type="submit" name="title"/></form>')
+    res.send('Express JS')
 })
 
-app.use('/', (req, res, next) => {
-    res.send('<form action="/product" method="POST"> <input type="submit" name="title"/></form>')
-})
+// app.use((req, res, next) => {
+//     console.log('In the Middleware');
+//     next();
+// });
 
-app.use((req, res, next) => {
-    console.log('In the Middleware');
-    next();
-});
-
-app.use((req, res, next) => {
-    console.log('In the Another Middleware');
-    res.send({ key1: 2 });
-});
+// app.use((req, res, next) => {
+//     console.log('In the Another Middleware');
+//     // res.send({ key1: 2 });
+//     next();
+// });
 
 const server = http.createServer(app);
 
