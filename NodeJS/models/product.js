2cca3c404baf83a2bc9ab3b9a61630d834261b46
RKY <xraj2023@gmail.com>
2024-03-13 20:09:33 +0530

    sql

--- NodeJS/models/product.js
+++ NodeJS/models/product.js
@@ -1,21 +1,22 @@
-const fs = require('fs');
-const path = require('path');
+// const fs = require('fs');
+// const path = require('path');
+const db = require('../util/database');
 
-const p = path.join(
-  path.dirname(process.mainModule.filename),
-  'data',
-  'products.json'
-);
+// const p = path.join(
+//   path.dirname(process.mainModule.filename),
+//   'data',
+//   'products.json'
+// );
 
-const getProductsFromFile = cb => {
-  fs.readFile(p, (err, fileContent) => {
-    if (err) {
-      cb([]);
-    } else {
-      cb(JSON.parse(fileContent));
-    }
-  });
-};
+// const getProductsFromFile = cb => {
+//   fs.readFile(p, (err, fileContent) => {
+//     if (err) {
+//       cb([]);
+//     } else {
+//       cb(JSON.parse(fileContent));
+//     }
+//   });
+// };
 
 module.exports = class Product {
   constructor(id, title, imageUrl, description, price) {
@@ -26,25 +27,31 @@ module.exports = class Product {
     this.price = price;
   }
 
-  save() {
-      getProductsFromFile(products => {
-        if (this.id) {
-              const existingProductIndex = products.findIndex(prod => prod.id === this.id);
-              const updateProducts = [...products];
-              updateProducts[existingProductIndex] = this;
-              fs.writeFile(p, JSON.stringify(updateProducts), err => {
-                console.log(err);
-              });
-        } else {
-            this.id = Math.random().toString();
-            products.push(this);
-            fs.writeFile(p, JSON.stringify(products), err => {
-              console.log(err);
-            });
-        }
-    });
-  }
+//   save() {
+//       getProductsFromFile(products => {
+//         if (this.id) {
+//               const existingProductIndex = products.findIndex(prod => prod.id === this.id);
+//               const updateProducts = [...products];
+//               updateProducts[existingProductIndex] = this;
+//               fs.writeFile(p, JSON.stringify(updateProducts), err => {
+//                 console.log(err);
+//               });
+//         } else {
+//             this.id = Math.random().toString();
+//             products.push(this);
+//             fs.writeFile(p, JSON.stringify(products), err => {
+//               console.log(err);
+//             });
+//         }
+//     });
+//   }
 
+  save () {
+    return db.execute('INSERT INTO products (title, price, imageUrl, description) VALUES (?, ?, ?, ?)',
+    [this.title, this.price, this.imageUrl, this.description]
+    );
+  }
+  
   delete() {
     getProductsFromFile( products => {
         if (this.id) {
@@ -56,14 +63,13 @@ module.exports = class Product {
     });
   }
 
-  static fetchAll(cb) {
-    getProductsFromFile(cb);
+  static fetchAll() {
+    return db.execute('SELECT * FROM products')
+    .then()
+    .catch();
   }
 
-  static findById(id, cb) {
-    getProductsFromFile( products => {
-        const product = products.find( p => p.id === id);
-        cb(product);
-    });
+  static findById(id) {
+    return db.execute('SELECT * FROM products WHERE products.id = ?',[id])
   }
 };
--- NodeJS/util/database.js
+++ NodeJS/util/database.js
@@ -3,6 +3,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
+    port: 3308,
     database: 'node-complete',
     password: '2023'
 });
